2d5b12b3b624cd619f5d753700b6683a19f8b3a0
Ellis Valentiner <ellisvalentiner@gmail.com>
2017-11-04 14:18:12 -0400

    Initial code

---
+++ src/utils.jl
@@ -0,0 +1,38 @@
+
+"""
+Identify NDT servers
+"""
+function get_ndt_servers(; policy::String="nearest")
+  @assert in(policy, ["nearest", "all"])
+  resp = Requests.get("https://mlab-ns.appspot.com/ndt"; query=Dict("policy"=>policy))
+  Requests.json(resp)
+end
+
+"""
+Run network test to specified server
+"""
+function run_test(fqdn::String)
+  read(`web100clt --name $fqdn`, String)
+end
+
+"""
+Parse web100clt output
+"""
+function parse_speeds(output::String)
+  speed = collect(Iterators.flatten(matchall.(r"\d+.\d+", matchall(r"\d+.\d+ Mb/s", output))))
+  return float.(speed)
+end
+
+"""
+
+"""
+function speed_test(;server_name::String=nothing)
+  if server_name === nothing
+    nearest_server = get_ndt_servers(policy="nearest")
+    server_name = nearest_server["fqdn"]
+  end
+  timestamp = now()
+  raw = run_test(server_name)
+  speeds = parse_speeds(raw)
+  return Dict("timestamp"=>timestamp, "upload"=>speeds[1], "download"=>speeds[2])
+end
