fe2c668e7a26ead533c69eebd63c12c2f17d15e2
Ellis Valentiner <ellisvalentiner@users.noreply.github.com>
2017-11-08 16:08:20 -0500

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ using Documenter, SpeedTest
 makedocs()
 
 deploydocs(
-    deps   = Deps.pip("mkdocs"),
+    deps   = Deps.pip("mkdocs", "python-markdown-math"),
     repo   = "github.com/ellisvalentiner/SpeedTest.jl.git",
     julia  = "0.6"
 )
